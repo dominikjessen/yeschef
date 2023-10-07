8f7a468b368b751304757cc03533dc39e649c985
Dominik <djessen94@gmail.com>
2023-10-07 21:16:48 +0200

    Edamam attribution (move to component later)

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,3 +1,11 @@
+import Image from 'next/image';
+import EdamamBadge from '/public/Edamam_Badge_Transparent.svg';
+
 export default async function Footer() {
-  return <footer className="bg-green-200 px-10 py-12">Footer</footer>;
+  return (
+    <footer className="bg-green-200 px-10 py-12">
+      <div>Footer</div>
+      <Image src={EdamamBadge} alt="Powered by Edamam" width={200} />
+    </footer>
+  );
 }
