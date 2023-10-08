06efb70ac8077bd1ea81d908a8d312ea19707c2d
Dominik <djessen94@gmail.com>
2023-10-08 17:33:06 +0200

    Change disabled +/- button thresholds

--- src/app/(with-nav)/mealplan/mealplan.tsx
+++ src/app/(with-nav)/mealplan/mealplan.tsx
@@ -189,12 +189,12 @@ export default function Mealplan() {
             </div>
             <Separator orientation="vertical" />
             <div className="flex gap-4">
-              <Button variant="icon" size="icon" onClick={removeOneRecipe} disabled={mealplans[current].length === 0} aria-label="Remove one day">
+              <Button variant="icon" size="icon" onClick={removeOneRecipe} disabled={mealplans[current].length <= 1} aria-label="Remove one day">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
                   <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 12h-15" />
                 </svg>
               </Button>
-              <Button variant="icon" size="icon" onClick={handleAddOneClicked} disabled={mealplans[current].length === 7} aria-label="Add one day">
+              <Button variant="icon" size="icon" onClick={handleAddOneClicked} disabled={mealplans[current].length >= 7} aria-label="Add one day">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
                   <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
                 </svg>
