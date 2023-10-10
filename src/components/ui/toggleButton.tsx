72007d2a9b7f3854d8d09632f218a4261133e1a2
Dominik <djessen94@gmail.com>
2023-10-10 17:29:05 +0200

    If toggle option is disabled no bg on hover

--- src/components/ui/toggleButton.tsx
+++ src/components/ui/toggleButton.tsx
@@ -19,7 +19,7 @@ const ToggleButtonOption = React.forwardRef<
     <RadioGroupPrimitive.Item
       ref={ref}
       className={cn(
-        'px-1 py-1 md:px-3 md:py-2 rounded data-[state=checked]:text-primary-foreground data-[state=checked]:bg-primary data-[state=unchecked]:bg-transparent data-[state=unchecked]:text-foreground data-[state=unchecked]:hover:bg-primary/10',
+        'px-1 py-1 md:px-3 md:py-2 rounded data-[state=checked]:text-primary-foreground data-[state=checked]:bg-primary data-[state=unchecked]:bg-transparent data-[state=unchecked]:text-foreground data-[state=unchecked]:hover:bg-primary/10 data-[state=unchecked]:disabled:hover:bg-transparent',
         className
       )}
       {...props}
