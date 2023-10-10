b647064669e1a10abde9bf714c8da09f69e55e3d
Dominik <djessen94@gmail.com>
2023-10-10 08:59:53 +0200

    Force dynamic for /mealplan

--- src/app/(with-nav)/mealplan/page.tsx
+++ src/app/(with-nav)/mealplan/page.tsx
@@ -2,6 +2,8 @@ import { getServerSession } from 'next-auth';
 import Mealplan from './mealplan';
 import { authOptions } from '@/app/api/auth/[...nextauth]/route';
 
+export const dynamic = 'force-dynamic';
+
 export default async function MealplanPage() {
   const session = await getServerSession(authOptions);
 
