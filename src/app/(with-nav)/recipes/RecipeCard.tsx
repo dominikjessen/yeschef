5767ca649bf638d2cee0cc291ae41f2f8866fe1b
Dominik <djessen94@gmail.com>
2023-10-15 12:17:49 +0200

    Recipe page styling

---
+++ src/app/(with-nav)/recipes/RecipeCard.tsx
@@ -0,0 +1,56 @@
+'use client';
+
+import { Recipe } from '@prisma/client';
+import { motion } from 'framer-motion';
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '@/components/ui/tooltip';
+import { useRouter } from 'next/navigation';
+
+export default function RecipeCard({ recipe, index }: { recipe: Recipe; index: number }) {
+  const router = useRouter();
+
+  function openRecipePage() {
+    router.push(`/recipes/${recipe.id}`);
+  }
+
+  return (
+    <motion.div
+      className="flex justify-between gap-4 items-center py-4 px-6 border rounded-lg hover:border-primary hover:shadow hover:bg-primary/10"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ delay: 0.05 * index }}
+      role="button"
+      onClick={openRecipePage}
+    >
+      <h3 className="font-bold text-base md:text-xl line-clamp-2 grow">{recipe.name}</h3>
+      <div className="flex gap-4 md:gap-6 items-center">
+        {recipe.url && (
+          <TooltipProvider>
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <a href={recipe.url} target="_blank" className="h-10 w-10 hover:bg-foreground/10 flex items-center justify-center rounded">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    className="w-6 h-6"
+                  >
+                    <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6" />
+                    <polyline points="15 3 21 3 21 9" />
+                    <line x1="10" x2="21" y1="14" y2="3" />
+                  </svg>
+                </a>
+              </TooltipTrigger>
+              <TooltipContent>
+                <p>Go to recipe source</p>
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
+        )}
+      </div>
+    </motion.div>
+  );
+}
--- src/app/(with-nav)/recipes/page.tsx
+++ src/app/(with-nav)/recipes/page.tsx
@@ -5,6 +5,79 @@ import { Recipe } from '@prisma/client';
 import { getServerSession } from 'next-auth/next';
 import Link from 'next/link';
 import { redirect } from 'next/navigation';
+import RecipeCard from './RecipeCard';
+
+function NoRecipes() {
+  return (
+    <div className="p-10 md:p-16 bg-primary/5 rounded-xl flex flex-col gap-10 md:gap-16 items-center">
+      <div className="flex gap-4 md:gap-8 text-inactive">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          className="w-12 h-12 md:w-16 md:h-16"
+        >
+          <path d="M3 11v3a1 1 0 0 0 1 1h16a1 1 0 0 0 1-1v-3" />
+          <path d="M12 19H4a1 1 0 0 1-1-1v-2a1 1 0 0 1 1-1h16a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1h-3.83" />
+          <path d="m3 11 7.77-6.04a2 2 0 0 1 2.46 0L21 11H3Z" />
+          <path d="M12.97 19.77 7 15h12.5l-3.75 4.5a2 2 0 0 1-2.78.27Z" />
+        </svg>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          className="w-12 h-12 md:w-16 md:h-16"
+        >
+          <path d="M2.27 21.7s9.87-3.5 12.73-6.36a4.5 4.5 0 0 0-6.36-6.37C5.77 11.84 2.27 21.7 2.27 21.7zM8.64 14l-2.05-2.04M15.34 15l-2.46-2.46" />
+          <path d="M22 9s-1.33-2-3.5-2C16.86 7 15 9 15 9s1.33 2 3.5 2S22 9 22 9z" />
+          <path d="M15 2s-2 1.33-2 3.5S15 9 15 9s2-1.84 2-3.5C17 3.33 15 2 15 2z" />
+        </svg>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          className="w-12 h-12 md:w-16 md:h-16"
+        >
+          <path d="M15 11h.01" />
+          <path d="M11 15h.01" />
+          <path d="M16 16h.01" />
+          <path d="m2 16 20 6-6-20A20 20 0 0 0 2 16" />
+          <path d="M5.71 17.11a17.04 17.04 0 0 1 11.4-11.4" />
+        </svg>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          className="w-12 h-12 md:w-16 md:h-16"
+        >
+          <circle cx="9" cy="7" r="2" />
+          <path d="M7.2 7.9 3 11v9c0 .6.4 1 1 1h16c.6 0 1-.4 1-1v-9c0-2-3-6-7-8l-3.6 2.6" />
+          <path d="M16 13H3" />
+          <path d="M16 17H3" />
+        </svg>
+      </div>
+      <p className="w-full lg:w-1/2 xl:w-1/3 text-foreground/50 font-bold text-base md:text-xl text-center leading-relaxed">
+        You have not created any recipes yet, add all your fan favorites now!
+      </p>
+    </div>
+  );
+}
 
 export default async function Recipes() {
   const session = await getServerSession(authOptions);
@@ -21,46 +94,15 @@ export default async function Recipes() {
   }
 
   return (
-    <div className="flex flex-col gap-8">
+    <div className="flex flex-col gap-6 md:gap-8">
       <div className="flex justify-between">
         <h2 className="font-bold text-2xl">Your recipes</h2>
         <Link href="/recipes/new">
           <Button>Add recipe</Button>
         </Link>
       </div>
-      <div className="grid grid-cols-5 gap-4">
-        {userRecipes.length ? (
-          userRecipes.map((recipe) => (
-            <div key={recipe.id} className="flex flex-col gap-6 items-center justify-center p-8 border border-primary shadow">
-              <h3 className="font-bold text-xl">{recipe.name}</h3>
-              {recipe.url && (
-                <a href={recipe.url} target="_blank">
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M13.19 8.688a4.5 4.5 0 011.242 7.244l-4.5 4.5a4.5 4.5 0 01-6.364-6.364l1.757-1.757m13.35-.622l1.757-1.757a4.5 4.5 0 00-6.364-6.364l-4.5 4.5a4.5 4.5 0 001.242 7.244"
-                    />
-                  </svg>
-                </a>
-              )}
-              <Link href={`/recipes/${recipe.id}`}>
-                <Button variant="outline">
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4">
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
-                    />
-                  </svg>
-                  <span className="ml-2">Edit recipe</span>
-                </Button>
-              </Link>
-            </div>
-          ))
-        ) : (
-          <div>No recipes yet, add some now!</div>
-        )}
+      <div className="grid grid-cols-1 gap-2 md:gap-4">
+        {userRecipes.length ? userRecipes.map((recipe, index) => <RecipeCard key={recipe.id} recipe={recipe} index={index} />) : <NoRecipes />}
       </div>
     </div>
   );
