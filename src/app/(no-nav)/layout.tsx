f46b3399be314ffe911190195129a3358b20c78f
Dominik <djessen94@gmail.com>
2025-11-01 19:29:21 +0100

    Add Vercel analytics

--- src/app/(no-nav)/layout.tsx
+++ src/app/(no-nav)/layout.tsx
@@ -1,19 +1,26 @@
-import '../globals.css';
-import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
+import "../globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next";
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Mealplan - Get your weekly food sorted',
-  description: 'Mealplan is an app that helps you figure out what to eat this week.'
+  title: "Yeschef - Get your weekly food sorted",
+  description:
+    "Yeschef is an app that helps you figure out what to eat this week.",
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
       <body className={`${inter.className} bg-background text-foreground`}>
         <main>{children}</main>
+        <Analytics />
       </body>
     </html>
   );
--- src/app/(with-nav)/layout.tsx
+++ src/app/(with-nav)/layout.tsx
@@ -1,27 +1,38 @@
-import '../globals.css';
-import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
-import HeaderWithNav from '@/components/layout/HeaderWithNav';
-import Footer from '@/components/layout/Footer';
-import { Toaster } from '@/components/ui/toaster';
+import "../globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import HeaderWithNav from "@/components/layout/HeaderWithNav";
+import Footer from "@/components/layout/Footer";
+import { Toaster } from "@/components/ui/toaster";
+import { Analytics } from "@vercel/analytics/next";
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Mealplan - Get your weekly food sorted',
-  description: 'Mealplan is an app that helps you figure out what to eat this week.'
+  title: "Yeschef - Get your weekly food sorted",
+  description:
+    "Yeschef is an app that helps you figure out what to eat this week.",
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-background text-foreground grid grid-flow-row md:grid-flow-col grid-cols-[min-content_1fr]`}>
+      <body
+        className={`${inter.className} bg-background text-foreground grid grid-flow-row md:grid-flow-col grid-cols-[min-content_1fr]`}
+      >
         <HeaderWithNav headerClassName="px-4 md:px-10 col-span-2 md:col-span-1" />
         <div className="col-span-2 md:col-span-1 w-full">
-          <main className="px-4 md:px-10 py-4 md:py-8 min-h-[calc(100dvh-58px)]">{children}</main>
+          <main className="px-4 md:px-10 py-4 md:py-8 min-h-[calc(100dvh-58px)]">
+            {children}
+          </main>
           <Toaster />
           <Footer className="px-4 md:px-10" />
         </div>
+        <Analytics />
       </body>
     </html>
   );
